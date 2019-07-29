43e02120594f2454234fb65f47424e7c856427ec
Aleksandr Chernyi <iblasterus@gmail.com>
2019-07-29 09:32:45 +0300

    Initial commit

---
+++ ProportionalHeap.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Proportional Heap it's a class that obtain association array with digit values and perform proportional balance to
+ * source value
+ *
+ * @author Aleksandr Chernyi <iblasterus@gmail.com>
+ * @date 07/29/2019
+ */
+class ProportionalHeap
+{
+
+}
